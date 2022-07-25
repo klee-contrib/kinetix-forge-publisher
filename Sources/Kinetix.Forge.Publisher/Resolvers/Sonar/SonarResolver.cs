a332b34f444bbb1d3d74843399d6059f61e10cb4
Sébastien EZRATTY <sebastien.ezratty@kleegroup.com>
2022-07-25 08:05:17 +0200

    Fix HTTPS Sonar issue.

--- Sources/Kinetix.Forge.Publisher/Resolvers/Sonar/SonarResolver.cs
+++ Sources/Kinetix.Forge.Publisher/Resolvers/Sonar/SonarResolver.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using Kinetix.Forge.Publisher.Dto;
 
 namespace Kinetix.Forge.Publisher.Resolvers.Sonar
@@ -10,7 +11,8 @@ namespace Kinetix.Forge.Publisher.Resolvers.Sonar
     {
         public SonarResolver()
         {
-
+            /* Gestion du TLS avec .NET obsolète. */
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | (SecurityProtocolType)768 /* Tls11 */ | (SecurityProtocolType)3072 /* Tls12 */ | (SecurityProtocolType)12288 /* Tls13 */;
         }
 
         /// <inheritdoc/>
